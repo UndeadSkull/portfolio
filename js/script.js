36d9916af069a6f1a924a14d92c6d587612e688d
Muhammed Favas <favasme08@gmail.com>
2021-02-24 01:16:13 +0530

    work tabs

--- js/script.js
+++ js/script.js
@@ -11,3 +11,15 @@ window.onscroll = function (e) {
     else if (vertical_position < 1200) your_div.style.setProperty("top", "50%");
     else your_div.style.setProperty("top", "81%");
 }
+function tab(val){
+    var all = document.getElementsByClassName("work-container");
+    var heading = document.getElementsByClassName("tab-heading");
+    var i;
+    for (i = 0; i < 3; i++) {
+        all[i].style.opacity = "0";
+        heading[i].classList.remove('active'); 
+    }   
+    var x = document.getElementById(val)
+    x.style.opacity = "1";
+    x.classList.add("active");
+  }
